61e842dc7b6daaf0d645d34afc0a815bbd7bef27
BenZen59 <benoit.zenone@gmail.com>
2023-04-03 11:03:34 +0200

    add Array and fast field

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FieldArray,
+  FastField,
+} from 'formik';
 import * as Yup from 'yup';
+import TextError from './TextError';
 
 const initialValues = {
   name: 'Benku',
@@ -8,6 +16,12 @@ const initialValues = {
   channel: 'benkuable',
   comments: '',
   address: '',
+  social: {
+    facebook: '',
+    twitter: '',
+  },
+  phoneNumbers: ['', ''],
+  phNumbers: [''],
 };
 
 const onSubmit = (values) => {
@@ -31,12 +45,14 @@ function YoutubeForm() {
         <div className='form-control'>
           <label htmlFor='name'>Name</label>
           <Field type='text' id='name' name='name' />
-          <ErrorMessage name='name' />
+          <ErrorMessage name='name' component={TextError} />
         </div>
         <div className='form-control'>
           <label htmlFor='name'>E-mail</label>
           <Field type='email' id='email' name='email' />
-          <ErrorMessage name='email' />
+          <ErrorMessage name='email'>
+            {(errorMsg) => <div className='error'>{errorMsg}</div>}
+          </ErrorMessage>
         </div>
         <div className='form-control'>
           <label htmlFor='channel'>Channel</label>
@@ -46,7 +62,7 @@ function YoutubeForm() {
             name='channel'
             placeholder='Youtube channel name'
           />
-          <ErrorMessage name='channel' />
+          <ErrorMessage name='channel' component={TextError} />
         </div>
         <div className='form-control'>
           <label htmlFor='comments'>Comments</label>
@@ -54,8 +70,9 @@ function YoutubeForm() {
         </div>
         <div className='form-control'>
           <label htmlFor='address'>Address</label>
-          <Field name='address' />
+          <FastField name='address' />
           {(props) => {
+            console.log('Field render');
             const { field, form, meta } = props;
             console.log('Render props', props);
             return (
@@ -65,6 +82,52 @@ function YoutubeForm() {
               </div>
             );
           }}
+        </div>
+        <div className='form-control'>
+          <label htmlFor='facebook'>Facebook profile</label>
+          <Field type='text' id='facebook' name='social.facebook' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='twitter'>Twitter profile</label>
+          <Field type='text' id='twitter' name='social.twitter' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='primaryPh'>Primary phone number</label>
+          <Field type='text' id='primaryPh' name='phoneNumbers[0]' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='secondaryPh'>Secondary phone number</label>
+          <Field type='text' id='secondaryPh' name='phoneNumbers[1]' />
+        </div>
+        <div className='form-control'>
+          <label>List of phone numbers</label>
+          <FieldArray name='phNumbers'>
+            {(fieldArrayProps) => {
+              console.log('fieldArrayProps', fieldArrayProps);
+              const { push, remove, form } = fieldArrayProps;
+              const { values } = form;
+              const { phNumbers } = values;
+              return (
+                <div>
+                  {phNumbers.map((phNumber, index) => (
+                    <div key={index}>
+                      <Field name={`phNumbers[${index}]`} />
+                      {index > 0 && (
+                        <button type='button' onClick={() => remove(index)}>
+                          {' '}
+                          -{' '}
+                        </button>
+                      )}
+                      <button type='button' onClick={() => push(index)}>
+                        {' '}
+                        +{' '}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
         </div>
         <button>Submit</button>
       </Form>
